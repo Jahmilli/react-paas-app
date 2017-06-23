6d58b459d44f9ddc6c23a30eb235a744440a8420
Sebastian Southern <sebastian.southern@gmail.com>
2017-06-24 06:10:20 +1000

    Navigation style fix
    Navigation has been built properly using bootstrap, will needed to be
    added onto later for actual functionality using indexlinkcontainer and
    React Router

--- my-app/src/header/Navigation.jsx
+++ my-app/src/header/Navigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 export default class Navigation extends React.Component {
 
@@ -7,22 +7,30 @@ export default class Navigation extends React.Component {
 render() {
 
   return (
-  <Navbar fixedTop>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a className="page-scroll" href="#page-top" >Some Brand</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav bsStyle="pills">
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
-        <NavItem eventKey={3} href="#">Link</NavItem>
-        <NavItem eventKey={4} href="#">Link</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  <Navbar fixedTop inverse>
+        <Navbar.Header>
+          <Navbar.Toggle />
+          <Navbar.Brand>
+            <p>PAAS</p>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+              <NavItem eventKey={1}>Home</NavItem>
+              <NavItem eventKey={2}>About</NavItem>
+              <NavItem eventKey={3}>Event</NavItem>
+              <NavItem eventKey={4}>Contact</NavItem>
+
+            <NavDropdown key={2} title="Home" id="about-dropdown">
+                <MenuItem eventKey={2.1}>Home</MenuItem>
+                <MenuItem eventKey={2.2}>About</MenuItem>
+                <MenuItem eventKey={2.3}>Events</MenuItem>
+              <MenuItem divider />
+                <MenuItem eventKey={2.4}>Contact</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 );
 }
 }
