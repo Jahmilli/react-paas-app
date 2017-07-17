f09aba11dca00fb127e0a50f837248134bf1c81e
Sebastian Southern <sebastian.southern@gmail.com>
2017-07-17 14:06:03 +1000

    Minor clean up

--- my-app/src/body/FutureEvents.jsx
+++ my-app/src/body/FutureEvents.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import { Col } from 'react-bootstrap';
 import { Parallax } from 'react-parallax';
-import { Link } from 'react-router-dom';
-
 
 export default class FutureEvents extends Component {
   render() {
--- my-app/src/body/PastEvents.jsx
+++ my-app/src/body/PastEvents.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Parallax } from 'react-parallax';
-import { Link } from 'react-router-dom';
 
 export default class PastEvents extends Component {
   render() {
