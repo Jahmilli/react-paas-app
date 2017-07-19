41dabe176ff455abfd002ba03f4cb8b085b6658a
Sebastian Southern <sebastian.southern@gmail.com>
2017-07-19 22:33:04 +1000

    Attempt to make photo gallery more efficient
    it failed :( the site still renders properly and the object still maps
    out the url but its not getting passed down as a prop the way I need,
    work on it in the future or try ask for help!

--- my-app/src/body/PhotoGallery.jsx
+++ my-app/src/body/PhotoGallery.jsx
@@ -5,11 +5,30 @@ import GalleryThumbnail from './GalleryThumbnail';
 import ContainedElement from './ContainedElement';
 
 export default class PhotoGallery extends Component {
+
+renderPhotos() {
+  const PHOTOLIST = [
+    {url: "https://scontent-syd2-1.xx.fbcdn.net/v/t31.0-8/19780611_1573388502705170_135582896334775158_o.jpg?oh=90d3dab781173876d83cce2ef3572fbc&oe=5A02981B"},
+    {url: "https://scontent-syd2-1.xx.fbcdn.net/v/t31.0-8/18278600_1503656399678381_3480088402011775015_o.jpg?oh=066492ad23ab20bf98459aa279047982&oe=5A1141E8"},
+    {url: "https://scontent-syd2-1.xx.fbcdn.net/v/t31.0-8/19780611_1573388502705170_135582896334775158_o.jpg?oh=90d3dab781173876d83cce2ef3572fbc&oe=5A02981B"},
+    {url: "https://scontent-syd2-1.xx.fbcdn.net/v/t31.0-8/19780611_1573388502705170_135582896334775158_o.jpg?oh=90d3dab781173876d83cce2ef3572fbc&oe=5A02981B"},
+  ];
+
+  var photoSelection = PHOTOLIST.map(x => {
+    return (
+    <Col className="GalleryImageLockup" md={3} style={{height: "200"}}>
+      <GalleryThumbnail
+      photo={x.url} />
+    </Col>
+    );
+  });
+}
   render() {
     return (
       <div className="container-fluid">
         <h2>Photo Gallery</h2>
         <div className="row">
+          {this.renderPhotos()}
           <Col className="GalleryImageLockup" md={3} style={{height: "200"}}>
             <GalleryThumbnail
             photo="https://scontent-syd2-1.xx.fbcdn.net/v/t31.0-8/19780611_1573388502705170_135582896334775158_o.jpg?oh=90d3dab781173876d83cce2ef3572fbc&oe=5A02981B"/>
