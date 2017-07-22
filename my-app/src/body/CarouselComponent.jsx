70c160ac93d592d679962bab432f909ef6de25f8
Sebastian Southern <sebastian.southern@gmail.com>
2017-07-23 01:11:03 +1000

    Fixes to Carousel Component for maintenance efficiency in future
    Carousel Items are being mapped out from an object to make it more
    efficient. Can probably make it even more easier to maintain and make
    changes to but will likely need to use an additional function to check
    whether elements in array are null.

--- my-app/src/body/CarouselComponent.jsx
+++ my-app/src/body/CarouselComponent.jsx
@@ -4,45 +4,37 @@ import { Carousel } from 'react-bootstrap'
 
 export default class CarouselComponent extends Component {
 
+  renderCarouselList() {
+  const LIST = [
+    {photo: this.props.photo1, url: this.props.link1, heading: this.props.heading1, description: this.props.description1},
+    {photo: this.props.photo2, url: this.props.link2, heading: this.props.heading2, description: this.props.description2},
+    {photo: this.props.photo3, url: this.props.link3, heading: this.props.heading3, description: this.props.description3},
+    {photo: this.props.photo4, url: this.props.link4, heading: this.props.heading4, description: this.props.description4},
+  ];
 
-  
+  return LIST.map(items => {
+  var index = 0;
+    return (
+    <Carousel.Item className="CarouselComponent">
+      <img width={1000} height={500} alt="900x500" src={items.photo}/>
+      <Carousel.Caption>
+        <h3>{items.heading}</h3>
+        <p>{items.description}</p>
+        <a href={items.url} target="_blank">Check it out</a>
+      </Carousel.Caption>
+    </Carousel.Item>
+    );
+    index++;
+  });
+
+  }
+
 
   render() {
     return (
       <Carousel>
-    <Carousel.Item className="CarouselComponent">
-      <img width={1000} height={500} alt="900x500" src={this.props.photo1}/>
-      <Carousel.Caption>
-        <h3>{this.props.heading1}</h3>
-        <p>{this.props.description1}</p>
-        <a href={this.props.link1} target="_blank">Check it out</a>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item className="CarouselComponent">
-      <img width={1000} height={500} alt="900x500" src={this.props.photo2}/>
-      <Carousel.Caption>
-        <h3>{this.props.heading2}</h3>
-        <p>{this.props.description2}</p>
-        <a href={this.props.link2} target="_blank">Check it out</a>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item className="CarouselComponent">
-      <img width={1000} height={500} alt="900x500" src={this.props.photo3}/>
-      <Carousel.Caption>
-        <h3>{this.props.heading3}</h3>
-        <p>{this.props.description3}</p>
-        <a href={this.props.link3} target="_blank">Check it out</a>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item className="CarouselComponent">
-      <img width={1000} height={500} alt="900x500" src={this.props.photo4}/>
-      <Carousel.Caption>
-        <h3>{this.props.heading4}</h3>
-        <p>{this.props.description4}</p>
-        <a href={this.props.link4} target="_blank">Check it out</a>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
+      {this.renderCarouselList()}
+      </Carousel>
     );
   }
 }
